d024243dd1ab632aa20b8f2f87adfe40a26547d7
Jenni <jennkur@uef.fi>
2020-02-20 15:52:20 +0200

    Miikkan muutokset mukana

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,6 @@ import AppNavbar from './components/AppNavbar';
 import TarvikeLista from './components/TarvikeLista'
 import TarvikeModal from './components/TarvikeModal'
 import { Container } from 'reactstrap';
-<<<<<<< HEAD
-//import Sidebar from './components/Sidebar';
-=======
-
-
->>>>>>> 27eee5a32935599d8ec7c750d7fe0c91a80cd67c
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/authActions';
